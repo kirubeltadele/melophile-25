47c18a1891fb0739bcf68f32d8c3fedb896a6df4
SamiTGITLIT <eytila83@gmail.com>
2025-03-30 16:18:37 +0300

    Logo on Login/Registration pages.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,23 +1,15 @@
-
 import RegisterForm from "@/components/auth/RegisterForm";
+import generalLogo from "/general-logo.jpg";
 
 const Register = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-melophile-50 to-teal-50 flex flex-col justify-center items-center p-4">
       <div className="mb-8 text-center">
-        <div className="inline-flex items-center justify-center p-2 bg-melophile-600 rounded-full mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M7 19a6 6 0 0 0 5.7-4"></path>
-            <path d="M15 4a6 6 0 0 0-5.7 4"></path>
-            <path d="M13 16l-3-2"></path>
-            <path d="M4 12h7"></path>
-            <path d="M15 9l-3 2"></path>
-            <path d="M13 7l3 2"></path>
-            <path d="M13 12h7"></path>
-            <path d="M19 16l-3-2"></path>
-            <path d="M19 8l-3 2"></path>
-          </svg>
-        </div>
+        <img 
+          src={generalLogo} 
+          alt="Melophile" 
+          className="h-80 w-auto mb-8 mx-auto"
+        />
         <h1 className="text-3xl font-bold text-melophile-800">Join Melophile</h1>
         <p className="text-gray-600">Create your account to get started</p>
       </div>
